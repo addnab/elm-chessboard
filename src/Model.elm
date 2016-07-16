20ed78563267250231fcf722aaa967ca097ac7f4
Abdud Dayan Adeeb <addnab@gmail.com>
2016-07-16 15:04:35 +0530

    Restructuring Model for readability

--- src/Model.elm
+++ src/Model.elm
@@ -16,47 +16,43 @@ createSquare : Rank -> File -> Maybe PlayerPiece -> Square
 createSquare rank file piece =
   Square piece { file = file, rank = rank } Nothing
 
-createRank : Rank -> List (Maybe PlayerPiece) -> List Square
+createRank : Rank -> List (Maybe PlayerPiece) -> Dict.Dict Int Square
 createRank rank pieceOrder =
-  List.map2
-    (createSquare rank)
-    [1..8]
-    pieceOrder
+  pieceOrder
+    |> List.map2 (createSquare rank) [1..8]
+    |> List.map2 (,) [1..8]
+    |> Dict.fromList
 
 emptyRank : Rank -> Dict.Dict Int Square
 emptyRank rank =
-  Dict.fromList
-    <| List.map2 (,) [1..8]
-      <| createRank rank
-        <| List.repeat 8 Nothing
+  List.repeat 8 Nothing
+    |> createRank rank
 
 initialFirstRank : Player -> Dict.Dict Int Square
 initialFirstRank player =
   let
     pieceOrder = List.map (PlayerPiece player) [ R, N, B, K, Q, B, N, R ]
-    rankNumber =
+    rank =
       case player of
         White -> 1
         Black -> 8
   in
-    Dict.fromList
-      <| List.map2 (,) [1..8]
-        <| createRank rankNumber
-          <| List.map Just pieceOrder
+    pieceOrder
+      |> List.map Just
+      |> createRank rank
 
 initialSecondRank : Player -> Dict.Dict Int Square
 initialSecondRank player =
   let
-    rankNumber =
+    rank =
       case player of
         White -> 2
         Black -> 7
     pieceOrder = List.map (PlayerPiece player) [ P, P, P, P, P, P, P, P ]
   in
-    Dict.fromList
-      <| List.map2 (,) [1..8]
-        <| createRank rankNumber
-          <| List.map Just pieceOrder
+    pieceOrder
+      |> List.map Just
+      |> createRank rank
 
 initial : Model
 initial =
